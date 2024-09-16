33fc926400afdef9711f306fda57ce6a007737eb
Cláudio Peruna <danielperuna2012@gmail.com>
2024-09-16 13:50:11 -0300

    declaração anti-plágio

--- src/vendaingressos/Usuario.java
+++ src/vendaingressos/Usuario.java
@@ -1,3 +1,15 @@
+/*******************************
+ Autor: Cláudio Daniel Figueredo Peruna
+ Componente Curricular: EXA863 - MI - PROGRAMAÇÃO
+ Concluído em: 15/09/2024
+ Declaro que este código foi elaborado por mim de forma individual e não contêm nenhum
+ trecho de código de outro colega ou de outro autor, tais como provindos de livros e
+ apostilas, e páginas ou documentos eletrônicos da Internet. Qualquer trecho de código
+ de outra autoria que não a minha está destacado com uma citação para o autor e a fonte
+ do código, e estou ciente que estes trechos não serão considerados para fins de avaliação.
+
+ ********************************/
+
 package vendaingressos;
 
 import java.util.List;
